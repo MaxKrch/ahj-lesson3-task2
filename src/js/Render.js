8c9d6db8f43fc2253ada303835d8942a483cbbe2
Max Kyrchanov <max.kyrchanov@gmail.com>
2024-01-16 16:35:17 +0500

    Complete Description Classes

--- src/js/Render.js
+++ src/js/Render.js
@@ -1,5 +1,52 @@
 export default class Render {
-	constructor () {
+	constructor (container) {
+		this.container = container;
+		this.sectionPinnedTasks;
+		this.sectionOtherTasks;
 		
+	}
+
+	renderPage() {
+
+	}
+
+	renderModal() {
+
+	}
+
+	renderButtons() {
+
+	}
+
+	showMessage(message, section) {
+
+	} 
+
+	hideMessage() {
+
+	}
+
+	showInterfaceButtons(idTask) {
+
+	}
+
+	hideInterfaceButtons() {
+
+	}
+
+	showMessageEmptySection(message, section) {
+	
+	}
+
+	hideMessageEmptySection(section) {
+		
+	}
+
+	renderSectionTasks(tasks, section) {
+
+	}
+
+	renderTask(task, section) {
+
 	}
 }
--- src/js/State.js
+++ src/js/State.js
@@ -1,6 +1,26 @@
 export default class State {
 	constructor () {
 		this.idNewTask = 0;
+		this.pinnedTasks = [];
+		this.otherTasks = [];
+		this.hiddenTasks = [];
+		this.tempTextNewTask = null;
+	}
+
+	saveBoard() {
+//тут будет основная логика сохранения текущего стейта в локалсторэдж
+
+		return false;
+	}
+
+	loadBoard () {
+		const savedBoard = [];
+
+		return savedBoard;
+//тут по аналоги основная логика загрузки из локал стораджа, 
+	}
+
+	clearState() {
 		
 	}
 }
